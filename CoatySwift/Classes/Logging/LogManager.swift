016c8c8338a78098bdc60d099c5d36cb9fcf4a57
Sandra Grujovic <sandra.grujovic@siemens.com>
2019-04-29 17:05:54 +0200

    feat: Add log level config option
    Co-authored-by: Johannes Rohwer <johannes.rohwer@siemens.com>

--- CoatySwift/Classes/Logging/LogManager.swift
+++ CoatySwift/Classes/Logging/LogManager.swift
@@ -10,12 +10,14 @@ import XCGLogger
 /// Provides a global logger for the CoatySwift framework.
 class LogManager {
     
+    internal static var logLevel = XCGLogger.Level.error
+    
     internal static let log: XCGLogger = {
         let log = XCGLogger(identifier: "CoatySwift",
                                includeDefaultDestinations: false)
         
         let systemDestination = AppleSystemLogDestination(identifier: "CoatySwift.systemDestination")
-        systemDestination.outputLevel = .info
+        systemDestination.outputLevel = LogManager.logLevel
         systemDestination.showLogIdentifier = true
         systemDestination.showFunctionName = false
         systemDestination.showThreadName = false
@@ -29,5 +31,25 @@ class LogManager {
         
         return log
     }()
+    
+    static internal func getLogLevel(logLevel: LogLevel) -> XCGLogger.Level {
+        switch logLevel {
+        case .debug:
+            return XCGLogger.Level.debug
+        case .error:
+            return XCGLogger.Level.error
+        case .info:
+            return XCGLogger.Level.info
+        case .warning:
+            return XCGLogger.Level.warning
+        }
+    }
+}
+
+public enum LogLevel {
+    case debug
+    case info
+    case warning
+    case error
 }
 
