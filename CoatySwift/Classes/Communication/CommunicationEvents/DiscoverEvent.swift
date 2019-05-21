2f420453db00a018faa051ed236fab7bd57a02dd
Sandra Grujovic <sandra.grujovic@siemens.com>
2019-05-21 13:17:52 +0200

    chore: Remove Discover indirection

--- CoatySwift/Classes/Communication/CommunicationEvents/DiscoverEvent.swift
+++ CoatySwift/Classes/Communication/CommunicationEvents/DiscoverEvent.swift
@@ -16,8 +16,7 @@ public class DisoverEventFactory<Family: ObjectFamily> {
     ///     - externalId: the external ID to discover
     public static func withExternalId(eventSource: Component,
                                       externalId: String) -> DiscoverEvent<Family> {
-        let discover = Discover(externalId: externalId)
-        let discoverEventData = DiscoverEventData.createFrom(eventData: discover)
+        let discoverEventData = DiscoverEventData(externalId: externalId)
         return DiscoverEvent(eventSource: eventSource, eventData: discoverEventData)
     }
     
@@ -31,8 +30,7 @@ public class DisoverEventFactory<Family: ObjectFamily> {
     public static func withExternalIdAndCoreTypes(eventSource: Component,
                                                   externalId: String,
                                                   coreTypes: [CoreType]) -> DiscoverEvent<Family> {
-        let discover = Discover(externalId: externalId, coreTypes: coreTypes)
-        let discoverEventData = DiscoverEventData.createFrom(eventData: discover)
+        let discoverEventData = DiscoverEventData(externalId: externalId, coreTypes: coreTypes)
         return DiscoverEvent(eventSource: eventSource, eventData: discoverEventData)
     }
     
@@ -46,8 +44,7 @@ public class DisoverEventFactory<Family: ObjectFamily> {
     public static func withExternalIdAndObjectTypes(eventSource: Component,
                                                     externalId: String,
                                                     objectTypes: [String]) -> DiscoverEvent<Family> {
-        let discover = Discover(externalId: externalId, objectTypes: objectTypes)
-        let discoverEventData = DiscoverEventData.createFrom(eventData: discover)
+        let discoverEventData = DiscoverEventData(externalId: externalId, objectTypes: objectTypes)
         return DiscoverEvent(eventSource: eventSource, eventData: discoverEventData)
     }
     
@@ -58,8 +55,7 @@ public class DisoverEventFactory<Family: ObjectFamily> {
     ///   - objectId: the object ID to discover
     public static func withObjectId(eventSource: Component,
                                     objectId: CoatyUUID) -> DiscoverEvent<Family> {
-        let discover = Discover(objectId: objectId)
-        let discoverEventData = DiscoverEventData.createFrom(eventData: discover)
+        let discoverEventData = DiscoverEventData(objectId: objectId)
         return DiscoverEvent(eventSource: eventSource, eventData: discoverEventData)
     }
     
@@ -73,8 +69,7 @@ public class DisoverEventFactory<Family: ObjectFamily> {
     public static func withExternalAndObjectId(eventSource: Component,
                                                externalId: String,
                                                objectId: CoatyUUID) -> DiscoverEvent<Family> {
-        let discover = Discover(objectId: objectId, externalId: externalId)
-        let discoverEventData = DiscoverEventData.createFrom(eventData: discover)
+        let discoverEventData = DiscoverEventData(externalId: externalId, objectId: objectId)
         return DiscoverEvent(eventSource: eventSource, eventData: discoverEventData)
     }
     
@@ -85,8 +80,7 @@ public class DisoverEventFactory<Family: ObjectFamily> {
     ///   - coreTypes: coreTypes the core types to discover
     public static func withCoreTypes(eventSource: Component,
                                      coreTypes: [CoreType]) -> DiscoverEvent<Family> {
-        let discover = Discover(coreTypes: coreTypes)
-        let discoverEventData = DiscoverEventData.createFrom(eventData: discover)
+        let discoverEventData = DiscoverEventData(coreTypes: coreTypes)
         return DiscoverEvent(eventSource: eventSource, eventData: discoverEventData)
     }
     
@@ -97,8 +91,7 @@ public class DisoverEventFactory<Family: ObjectFamily> {
     ///   - objectTypes: the object types to discover
     public static func withObjectTypes(eventSource: Component,
                                        objectTypes: [String]) -> DiscoverEvent<Family> {
-        let discover = Discover(objectTypes: objectTypes)
-        let discoverEventData = DiscoverEventData.createFrom(eventData: discover)
+        let discoverEventData = DiscoverEventData(objectTypes: objectTypes)
         return DiscoverEvent(eventSource: eventSource, eventData: discoverEventData)
     }
 }
@@ -148,31 +141,128 @@ public class DiscoverEvent<Family: ObjectFamily>: CommunicationEvent<DiscoverEve
 /// for a DiscoverEventData.
 public class DiscoverEventData: CommunicationEventData {
     
-    // MARK: - Public attributes.
+    // MARK: - Attributes.
     
-    public var object: Discover
-    
+    public var externalId: String?
+    public var objectId: CoatyUUID?
+    public var objectTypes: [String]?
+    public var coreTypes: [CoreType]?
+
     // MARK: - Initializers.
     
-    private init(object: Discover) {
-        self.object = object
+    /// Create a Discover instance for the given IDs or types.
+    ///
+    /// The following combinations of parameters are valid (use undefined for unused parameters):
+    /// - externalId can be used exclusively or in combination with objectId property.
+    ///  Only if used exclusively it can be combined with objectTypes or coreTypes properties.
+    /// - objectId can be used exclusively or in combination with externalId property.
+    ///  Must not be used in combination with objectTypes or coreTypes properties.
+    /// - objectTypes must not be used with objectId property.
+    ///  Should not be used in combination with coreTypes.
+    /// - coreTypes must not be used with objectId property.
+    /// Should not be used in combination with objectTypes.
+    ///
+    /// - Parameters:
+    ///     - externalId: The external ID of the object(s) to be discovered or undefined.
+    ///     - objectId: The internal UUID of the object to be discovered or undefined.
+    ///     - objectTypes: Restrict objects by object types (logical OR).
+    ///     - coreTypes: Restrict objects by core types (logical OR).
+    private init(externalId: String?, objectId: CoatyUUID?, objectTypes: [String]?, coreTypes: [CoreType]?) {
+        self.externalId = externalId
+        self.objectId = objectId
+        self.objectTypes = objectTypes
+        self.coreTypes = coreTypes
         super.init()
     }
     
-    static func createFrom(eventData: Discover) -> DiscoverEventData {
-        return DiscoverEventData(object: eventData)
+    // MARK: - Convenience initializers that cover all permitted parameter combinations.
+    
+    required init(objectId: CoatyUUID) {
+        self.objectId = objectId
+        super.init()
+    }
+    
+    required init(externalId: String, objectTypes: [String], coreTypes: [CoreType]) {
+        self.externalId = externalId
+        self.objectTypes = objectTypes
+        self.coreTypes = coreTypes
+        super.init()
+    }
+    
+    required init(externalId: String, objectTypes: [String]) {
+        self.externalId = externalId
+        self.objectTypes = objectTypes
+        super.init()
+    }
+    
+    required init(externalId: String, coreTypes: [CoreType]) {
+        self.externalId = externalId
+        self.coreTypes = coreTypes
+        super.init()
+    }
+    
+    required init(externalId: String) {
+        self.externalId = externalId
+        super.init()
+    }
+    
+    required init(objectId: CoatyUUID, externalId: String) {
+        self.objectId = objectId
+        self.externalId = externalId
+        super.init()
+    }
+    
+    required init(coreTypes: [CoreType], objectTypes: [String]) {
+        self.coreTypes = coreTypes
+        self.objectTypes = objectTypes
+        super.init()
+    }
+    
+    required init(coreTypes: [CoreType]) {
+        self.coreTypes = coreTypes
+        super.init()
+    }
+    
+    required init(objectTypes: [String]) {
+        self.objectTypes = objectTypes
+        super.init()
+    }
+    
+    required init(externalId: String, objectId: CoatyUUID) {
+        self.externalId = externalId
+        self.objectId = objectId
+        super.init()
     }
     
     // MARK: - Codable methods.
     
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        self.object = try container.decode(Discover.self)
+    enum DiscoverKeys: String, CodingKey {
+        case externalId
+        case objectId
+        case objectTypes
+        case coreTypes
+    }
+    
+    required public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: DiscoverKeys.self)
+        
+        // Decode attributes.
+        externalId = try container.decodeIfPresent(String.self, forKey: .externalId)
+        objectId = try container.decodeIfPresent(CoatyUUID.self, forKey: .objectId)
+        coreTypes = try container.decodeIfPresent([CoreType].self, forKey: .coreTypes)
+        objectTypes = try container.decodeIfPresent([String].self, forKey: .objectTypes)
         try super.init(from: decoder)
     }
     
     public override func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        try container.encode(self.object)
+        try super.encode(to: encoder)
+        var container = encoder.container(keyedBy: DiscoverKeys.self)
+        
+        // Encode attributes.
+        try container.encodeIfPresent(externalId, forKey: .externalId)
+        try container.encodeIfPresent(objectId, forKey: .objectId)
+        try container.encodeIfPresent(coreTypes, forKey: .coreTypes)
+        try container.encodeIfPresent(objectTypes, forKey: .objectTypes)
     }
+    
 }
