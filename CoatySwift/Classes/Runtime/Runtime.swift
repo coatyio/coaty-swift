ab9ba582c6821fcdddb0993533cf2cd5aba43946
Sandra Grujovic <sandra.grujovic@siemens.com>
2019-09-30 09:38:03 +0200

    fix: remove unneeded runtime attributes

--- CoatySwift/Classes/Runtime/Runtime.swift
+++ CoatySwift/Classes/Runtime/Runtime.swift
@@ -11,12 +11,6 @@ import Foundation
 /// information.
 public class Runtime {
     
-    // TODO: These constants are injected while building the framework.
-    // Auto-generated constants!
-    private (set) public static var FRAMEWORK_PACKAGE_NAME = "coaty"
-    private (set) public static var FRAMEWORK_PACKAGE_VERSION = "1.4.1"
-    private (set) public static var FRAMEWORK_BUILD_DATE = 1554114439732
-    
     /// Common options specified in container configuration.
     private (set) public var commonOptions: CommonOptions?
     
